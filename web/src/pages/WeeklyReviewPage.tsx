4195d1890918b5c08319ff7801422a1ff1f72127
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-09-07 15:58:48 +0530

    feat: add weekly review page and navigation

---
+++ web/src/pages/WeeklyReviewPage.tsx
@@ -0,0 +1,9 @@
+import { WeeklyReview } from '../components/WeeklyReview';
+
+export default function WeeklyReviewPage() {
+  return (
+    <div className="p-4">
+      <WeeklyReview />
+    </div>
+  );
+}
