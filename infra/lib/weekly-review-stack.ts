0f1008ebd1c2ca6cd992c127aaf0d460ed13c392
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-09-07 18:21:54 +0530

    feat: add weekly review stack

---
+++ infra/lib/weekly-review-stack.ts
@@ -0,0 +1,58 @@
+import {
+  Stack,
+  StackProps,
+  Duration,
+  aws_lambda as lambda,
+  aws_lambda_nodejs as lambdaNodejs,
+  aws_events as events,
+  aws_events_targets as targets,
+  aws_ssm as ssm,
+  aws_s3 as s3,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
+
+interface WeeklyReviewStackProps extends StackProps {
+  bucket: s3.IBucket;
+}
+
+export class WeeklyReviewStack extends Stack {
+  constructor(scope: Construct, id: string, props: WeeklyReviewStackProps) {
+    super(scope, id, props);
+
+    const openAiParam = ssm.StringParameter.fromStringParameterName(
+      this,
+      'OpenAIKey',
+      'openai-key'
+    );
+
+    const fn = new lambdaNodejs.NodejsFunction(this, 'WeeklyReviewFunction', {
+      functionName: 'weekly-review',
+      entry: path.join(__dirname, '../functions/weekly-review.ts'),
+      handler: 'handler',
+      runtime: lambda.Runtime.NODEJS_20_X,
+      timeout: Duration.minutes(15),
+      environment: {
+        BUCKET_NAME: props.bucket.bucketName,
+        OPENAI_KEY_PARAM: openAiParam.parameterName,
+      },
+    });
+
+    props.bucket.grantReadWrite(fn);
+    openAiParam.grantRead(fn);
+
+    fn.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['ssm:GetParameter'],
+        resources: [openAiParam.parameterArn],
+      })
+    );
+
+    const rule = new events.Rule(this, 'WeeklyReviewSchedule', {
+      schedule: events.Schedule.cron({ weekDay: 'MON', hour: '0', minute: '0' }),
+    });
+
+    rule.addTarget(new targets.LambdaFunction(fn));
+  }
+}
